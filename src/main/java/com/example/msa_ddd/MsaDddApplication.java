3471efd211d40ebf525c544a6cf87f8d19a6830d
kiku99 <elsd0326@gmail.com>
2022-08-12 17:44:43 +0900

    setting

---
+++ src/main/java/com/example/msa_ddd/MsaDddApplication.java
@@ -0,0 +1,13 @@
+package com.example.msa_ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaDddApplication.class, args);
+    }
+
+}
